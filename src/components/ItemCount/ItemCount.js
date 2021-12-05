93dcb1eea95198ef79bb920af7439e6216efed2d
Lucia Barrientos <luci980912@gmail.com>
2021-12-05 20:17:26 -0300

    se agrega useEffect

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -1,21 +1,24 @@
 import './ItemCount.css';
-import React,{useState} from 'react'
+import React,{useState, useEffect} from 'react'
 
 
 export default function Product() {
-
     const [clicks, setClicks] = useState(0);
+
+    useEffect(() => {
+        console.log('Ejecuta useEffect')
+    })
 
     const more = () => {
         setClicks(clicks + 1);
     };
-
     const less = () => {
         setClicks(clicks - 1);
     }
 
     return(
         <div id="contador" >
+            {console.log('Render')}
             <button onClick={less}>Quitar</button>
             <h4>
                 {clicks}
